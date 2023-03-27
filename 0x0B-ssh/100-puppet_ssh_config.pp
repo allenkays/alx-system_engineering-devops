ddacd18d86fac6880adfd7798a970714fd04fc90
allenkays <allenkays@tutanota.com>
2023-03-27 07:13:16 -0400

    Edit ssh config file using puppet

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,9 @@
+# using Puppet to make changes to our configuration file
+# SSH client configuration must be configured to use the private key ~/.ssh/school
+# SSH client configuration must be configured to refuse to authenticate using a password
+
+file_line {'Declare identity file and turn off passwd auth':
+  path => '/etc/ssh/ssh_config',
+  line => 'IdentityFile ~/.ssh/school',
+  line => 'PasswordAuthentication no',
+}
