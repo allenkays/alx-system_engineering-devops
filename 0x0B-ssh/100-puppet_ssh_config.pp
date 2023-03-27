afabd56261add71b3c33c5b9eced0a63d1564f6b
allenkays <allenkays@tutanota.com>
2023-03-27 07:17:33 -0400

    Separate the disabling of passwd auth and file identity

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -2,8 +2,11 @@
 # SSH client configuration must be configured to use the private key ~/.ssh/school
 # SSH client configuration must be configured to refuse to authenticate using a password
 
-file_line {'Declare identity file and turn off passwd auth':
+file_line {'Declare identity file':
   path => '/etc/ssh/ssh_config',
   line => 'IdentityFile ~/.ssh/school',
+}
+file_line{'Turn off passwd auth':
+  path => '/etc/ssh/ssh_config',
   line => 'PasswordAuthentication no',
 }
