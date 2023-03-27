ff2afa7c75a40d90bc4c6fb465cfc59165b590bf
allenkays <allenkays@tutanota.com>
2023-03-27 17:23:01 -0400

    webserver installation with puppet

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,21 @@
+# Setup nginx server
+
+package { 'nginx':
+  ensure     => 'present',
+}
+
+file { '/var/www/nginx/html/index.html':
+  content => 'Hello World!',
+}
+
+file_line { 'File':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 default_server;',
+  line   => 'rewrite ^/redirect_me https://www.google.com/ permanent;',
+}
+
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
+}
