e4aaf52329c7573842119e93315986c3d3229c70
allenkays <allenkays@tutanota.com>
2023-03-27 04:48:58 -0400

    Remove tabs and add double quotes and add commas

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,9 @@
+#create a file using puppet in /tmp
+
 file { 'school':
-path    => '/tmp/school'
-owner   => 'www-data'
-mode    => '0744'
-group   => 'www-data'
-content => "I love puppet"
+  path    => '/tmp/school',
+  owner   => 'www-data',
+  mode    => '0744',
+  group   => 'www-data',
+  content => 'I love puppet',
 }
