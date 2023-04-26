99e8a8006d60b061e1d9c6b1b8798be58168d6f3
allenkays <allenkays@tutanota.com>
2023-04-26 18:09:29 -0400

    Employee todo list

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -7,6 +7,38 @@ using REST API
 import requests
 import sys
 
+EMPLOYEE_API_URL = 'https://jsonplaceholder.typicode.com/users/{}'
+TODO_API_URL = 'https://jsonplaceholder.typicode.com/todos?userId={}'
 
+def get_employee_name(employee_id):
+    response = requests.get(EMPLOYEE_API_URL.format(employee_id))
+    employee_data = response.json()
+    return employee_data['name']
 
+def get_todo_list(employee_id):
+    response = requests.get(TODO_API_URL.format(employee_id))
+    return response.json()
 
+def display_todo_list_progress(employee_id):
+    employee_name = get_employee_name(employee_id)
+    todo_list = get_todo_list(employee_id)
+
+    total_tasks = len(todo_list)
+    done_tasks = len([task for task in todo_list if task['completed']])
+
+    print("Employee {} is done with tasks({}/{}):".format(employee_name, done_tasks, total_tasks))
+
+    for task in todo_list:
+        if task['completed']:
+            print("\t- {}".format(task['title']))
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print('Usage: python {} <employee_id>'.format(sys.argv[0]))
+        sys.exit(1)
+
+    try:
+        employee_id = int(sys.argv[1])
+    except ValueError:
+        print('Error: Invalid employee ID')
+        sys.exit(1)
