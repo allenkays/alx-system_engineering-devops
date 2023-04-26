2b5070f1b6e424a14400bdc39fe4ef73c76f9dfd
allenkays <allenkays@tutanota.com>
2023-04-26 18:35:51 -0400

    update extend to CSV

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,32 +1,41 @@
 #!/usr/bin/python3
-'''
-This Python script that returns information using REST API
-'''
+"""
+A Python script that returns information using REST API
+"""
+
 import requests
 import csv
 from sys import argv
 
 if __name__ == "__main__":
     if len(argv) > 1:
-        user = argv[1]
+        user_id = argv[1]
         url = "https://jsonplaceholder.typicode.com/"
-        r = requests.get("{}users/{}".format(url, user))
-        name = r.json().get("username")
+        r = requests.get("{}users/{}".format(url, user_id))
+        name = r.json().get("name")
+
         if name is not None:
             req = requests.get(
                 "{}todos?userId={}".format(
-                    url, user)).json()
-            tasks = []
-            for t in req:
-                task = {}
-                task["USER_ID"] = user
-                task["USERNAME"] = name
-                task["TASK_COMPLETED_STATUS"] = t.get("completed")
-                task["TASK_TITLE"] = t.get("title")
-                tasks.append(task)
-            with open("{}.csv".format(user), "w", newline="") as f:
-                fieldnames = ["USER_ID", "USERNAME",
-                              "TASK_COMPLETED_STATUS", "TASK_TITLE"]
-                writer = csv.DictWriter(f, fieldnames=fieldnames)
-                writer.writeheader()
-                writer.writerows(tasks)
+                    url, user_id)).json()
+            tasks = len(req)
+            completed_tasks = []
+            for task in req:
+                if task.get("completed") is True:
+                    completed_tasks.append(task)
+            count = len(completed_tasks)
+
+            # Displaying the result in the required format
+            print("Employee {} is done with tasks({}/{}):"
+                  .format(name, count, tasks))
+            for title in completed_tasks:
+                print("\t {}".format(title.get("title")))
+
+            # Writing data to CSV file
+            with open("{}{}.csv".format(user_id, name), 'w', newline='') as csvfile:
+                taskwriter = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+                for task in req:
+                    taskwriter.writerow([user_id, name,
+                                         task.get('completed'),
+                                         task.get('title')])
+
