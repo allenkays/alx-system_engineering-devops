ffa61464b2dbcbc93ff5a5c1cc719a351a42274d
allenkays <allenkays@tutanota.com>
2023-04-26 19:25:10 -0400

    Export to Json

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,29 @@
+!/usr/bin/python3
+"""script to export data in the CSV format"""
+
+
+import csv
+import json
+import requests
+import sys
+
+if __name__ == "__main__":
+
+    uid = sys.argv[1]
+    url = requests.get("https://jsonplaceholder.typicode.com/users/{}"
+                       .format(uid))
+    name = url.json().get('username')
+    todos = requests.get('https://jsonplaceholder.typicode.com/todos')
+    todo = todos.json()
+    taskList = []
+    todo_All = {}
+    for i in todo:
+        if i.get('userId') == int(uid):
+            dict = {"task": i.get('title'),
+                    "completed": i.get('completed'),
+                    "username": url.json().get('username')}
+            taskList.append(dict)
+    todo_All[uid] = taskList
+    fileName = "{}.json".format(uid)
+    with open(fileName, mode='w') as jsn:
+        json.dump(todo_All, jsn)
