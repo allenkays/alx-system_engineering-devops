23de966a61f718f2b7395761f163a4bcb21c8059
allenkays <allenkays@tutanota.com>
2023-05-17 09:15:16 -0400

    Change file limits for user holberton

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,28 @@
+class open_file_limit {
+  $new_limit = 65535
+  $username = 'holberton'
+  $limits_conf = '/etc/security/limits.conf'
+  $limits_line = "$username soft nofile $new_limit\n$username hard nofile $new_limit"
+
+  package { 'sed':
+    ensure => installed,
+  }
+
+  file_line { 'increase_file_limits':
+    path    => $limits_conf,
+    line    => $limits_line,
+    match   => "^$username",
+    ensure  => present,
+    require => Package['sed'],
+    notify  => Exec['reload_sysctl'],
+  }
+
+  exec { 'reload_sysctl':
+    command     => '/sbin/sysctl -p',
+    refreshonly => true,
+  }
+
+  user { $username:
+    ensure => present,
+  }
+}
